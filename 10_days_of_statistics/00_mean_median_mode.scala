d41eb3a1fe8ad5051512498e26e18755ab71ccae
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2016-08-27 00:43:00 +0200

    solve day 0 of statistics

---
+++ 10_days_of_statistics/00_mean_median_mode.scala
@@ -0,0 +1,32 @@
+object Solution {
+
+  def main(args: Array[String]) {
+    val sc = new java.util.Scanner (System.in);
+    var n = sc.nextInt();
+    var arr = new Array[Int](n);
+    for(a_i <- 0 to n-1) {
+      arr(a_i) = sc.nextInt();
+    }
+
+    // Variables needed.
+    val l = arr.length
+    val arrSort = arr.sorted
+
+    // Calc mean
+    val mean = arr.sum / l.toFloat
+    println(mean)
+
+    // Sort array
+
+    // Calc median
+    val median = (arrSort(l/2-1) + arrSort(l/2-l%2)) / 2.0
+    println(median)
+
+    // Calc mode
+    val modeArr = arr.groupBy(identity).mapValues(_.size).toList.sortBy( x => (-x._2, x._1))
+    println(modeArr(0)._1)
+
+
+  }
+}
+

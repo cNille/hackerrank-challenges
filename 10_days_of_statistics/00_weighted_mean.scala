22e97fe3d2be77a8acee26e6407e063d5b2b8ceb
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2016-08-27 00:57:42 +0200

    solve day 0 weighted mean (statistics)

---
+++ 10_days_of_statistics/00_weighted_mean.scala
@@ -0,0 +1,20 @@
+object Solution {
+
+  def main(args: Array[String]) {
+    val sc = new java.util.Scanner (System.in);
+    var n = sc.nextInt();
+    var x = new Array[Int](n);
+    var w = new Array[Int](n);
+    for(a_i <- 0 to n-1) {
+      x(a_i) = sc.nextInt();
+    }
+    for(a_i <- 0 to n-1) {
+      w(a_i) = sc.nextInt();
+    }
+
+
+    val res = x.zip(w).map( x => x._1 * x._2).sum / w.sum.toFloat
+    val rounded = Math.round(res * 10.0) / 10.0
+    println(rounded)
+  }
+}
